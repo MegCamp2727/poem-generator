096933c07eb2d0b7b5250e1f9abd7107a901f652
MegCamp2727 <MegCamp2727@gmail.com>
2025-03-13 10:09:27 -0400

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ let context =
 "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and seperate each line with a <br />. Make sure to follow user instructions below and sign the poem with 'SheCodes AI' inside <strong> element";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating"> ⏳ Generating a poem about ${instructionsInput.value}..</div>`;
+
 console.log("Generating poem...");
 console.log(`Prompt: ${prompt}`);
 console.log(`Context: ${context}`);
